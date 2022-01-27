1bdafc87c3340d7cfbed5ba260c3351793813cd1
nimitsarup <40407218+nimitsarup@users.noreply.github.com>
2022-01-27 15:26:43 +0000

    Ns boilerplate (#2)
    * try checkin
    
    * initial checkin
    
    * add s3 support etc
    
    * auth
    
    * README
    
    * updates
    
    * merge conflicts

--- config/config_test.go
+++ config/config_test.go
@@ -18,13 +18,14 @@ func TestConfig(t *testing.T) {
 			})
 
 			Convey("Then the values should be set to the expected defaults", func() {
-				So(cfg.BindAddr, ShouldEqual, "localhost:24700")
-				So(cfg.ApiURL, ShouldResemble, "http://localhost:24700")
+				So(cfg.BindAddr, ShouldEqual, "localhost:27500")
+				So(cfg.ApiURL, ShouldResemble, "http://localhost:27500")
 				So(cfg.Brokers, ShouldResemble, []string{"localhost:9092", "localhost:9093", "localhost:9094"})
 				So(cfg.KafkaVersion, ShouldEqual, "1.0.2")
 				So(cfg.KafkaSecProtocol, ShouldEqual, "")
 				So(cfg.KafkaMaxBytes, ShouldEqual, 2000000)
-				So(cfg.InteractivesTopic, ShouldEqual, "interactives-visualisations")
+				So(cfg.InteractivesWriteTopic, ShouldEqual, "interactives-visualisations-api")
+				So(cfg.InteractivesReadTopic, ShouldEqual, "interactives-visualisations-importer")
 				So(cfg.GracefulShutdownTimeout, ShouldEqual, 5*time.Second)
 				So(cfg.HealthCheckInterval, ShouldEqual, 30*time.Second)
 				So(cfg.HealthCheckCriticalTimeout, ShouldEqual, 90*time.Second)
